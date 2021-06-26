b12ac2dd9b8bbf3ae4b574664ab77ce7bdf60aac
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2021-06-26 17:06:15 +0200

    perf(directions_possible): use bools over HashSet

--- src/directions_possible.rs
+++ src/directions_possible.rs
@@ -1,45 +1,54 @@
-use std::collections::HashSet;
-
 use crate::Direction;
 
+#[allow(clippy::struct_excessive_bools)]
 pub struct DirectionsPossible {
-    options: HashSet<Direction>,
+    options: usize,
+    left: bool,
+    right: bool,
+    up: bool,
+    down: bool,
 }
 
 #[allow(clippy::fn_params_excessive_bools)]
 impl DirectionsPossible {
     #[must_use]
     pub fn from_bools(left: bool, right: bool, up: bool, down: bool) -> Self {
-        let mut options = HashSet::new();
+        let mut options = 0;
         if left {
-            options.insert(Direction::Left);
+            options += 1;
         }
         if right {
-            options.insert(Direction::Right);
+            options += 1;
         }
         if up {
-            options.insert(Direction::Up);
+            options += 1;
         }
         if down {
-            options.insert(Direction::Down);
+            options += 1;
         }
-        Self { options }
+        Self {
+            options,
+            left,
+            right,
+            up,
+            down,
+        }
     }
 
     #[must_use]
     pub fn intersection(first: &Self, second: &Self) -> Self {
-        let options = first
-            .options
-            .intersection(&second.options)
-            .cloned()
-            .collect::<HashSet<_>>();
-        Self { options }
+        Self::from_bools(
+            first.left && second.left,
+            first.right && second.right,
+            first.up && second.up,
+            first.down && second.down,
+        )
     }
 
     #[must_use]
     pub fn single(&self) -> Option<&Direction> {
-        if self.options.len() == 1 {
-            self.options.iter().next()
+        if self.options == 1 {
+            self.any()
         } else {
             None
         }
@@ -47,23 +56,38 @@ impl DirectionsPossible {
 
     #[must_use]
     pub fn any(&self) -> Option<&Direction> {
-        self.options.iter().next()
+        if self.left {
+            Some(&Direction::Left)
+        } else if self.right {
+            Some(&Direction::Right)
+        } else if self.up {
+            Some(&Direction::Up)
+        } else if self.down {
+            Some(&Direction::Down)
+        } else {
+            None
+        }
     }
 
     #[must_use]
     pub fn contains(&self, direction: &Direction) -> bool {
-        self.options.contains(direction)
+        match direction {
+            Direction::Left => self.left,
+            Direction::Right => self.right,
+            Direction::Up => self.up,
+            Direction::Down => self.down,
+        }
     }
 
     #[must_use]
     pub fn is_empty(&self) -> bool {
-        self.options.is_empty()
+        self.options == 0
     }
 }
 
 impl core::fmt::Display for DirectionsPossible {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        if self.options.is_empty() {
+        if self.is_empty() {
             f.write_str("None")
         } else {
             f.write_str(if self.contains(&Direction::Left) {
