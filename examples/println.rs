3a3f3143217860a2ec788d617d40c35a5ce904ca
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2023-10-08 14:31:01 +0200

    refactor(example): remove never loop

--- examples/println.rs
+++ examples/println.rs
@@ -42,62 +42,61 @@ fn main() {
 
 fn snake(width: u8, height: u8) -> usize {
     let maximal_length_possible = u16::from(width) * u16::from(height);
-    loop {
-        let mut food = Point::random(width, height);
-        let mut snake = {
-            let start = Point::new(width / 2, height / 2);
-            let end = {
-                let x = if start.x < food.x {
-                    start.x - 1
-                } else {
-                    start.x + 1
-                };
-                Point::new(x, start.y)
+
+    let mut food = Point::random(width, height);
+    let mut snake = {
+        let start = Point::new(width / 2, height / 2);
+        let end = {
+            let x = if start.x < food.x {
+                start.x - 1
+            } else {
+                start.x + 1
             };
-            vec![start, end]
+            Point::new(x, start.y)
         };
+        vec![start, end]
+    };
 
-        loop {
-            print_field(width, height, &snake, food);
+    loop {
+        print_field(width, height, &snake, food);
 
-            let next_point = if let Some(point) = get_next_point(width, height, &snake, food) {
-                // Hits itself
-                if snake.contains(&point) {
-                    eprintln!("snake hit itself");
-                    return snake.len();
-                }
-
-                point
-            } else {
-                eprintln!("snake has nowhere to go");
+        let next_point = if let Some(point) = get_next_point(width, height, &snake, food) {
+            // Hits itself
+            if snake.contains(&point) {
+                eprintln!("snake hit itself");
                 return snake.len();
-            };
-
-            println!(
-                "snake length {:5}/{:5} goes to {:3} {:3}  food is at {:3} {:3}",
-                snake.len(),
-                maximal_length_possible,
-                next_point.x,
-                next_point.y,
-                food.x,
-                food.y
-            );
-
-            if next_point == food {
-                loop {
-                    let next_food = Point::random(width, height);
-                    // Dont create food on snake
-                    if !snake.contains(&next_food) {
-                        food = next_food;
-                        break;
-                    }
-                }
-            } else {
-                snake.pop().unwrap();
             }
 
-            snake.insert(0, next_point);
-            sleep(Duration::from_millis(200));
+            point
+        } else {
+            eprintln!("snake has nowhere to go");
+            return snake.len();
+        };
+
+        println!(
+            "snake length {:5}/{:5} goes to {:3} {:3}  food is at {:3} {:3}",
+            snake.len(),
+            maximal_length_possible,
+            next_point.x,
+            next_point.y,
+            food.x,
+            food.y
+        );
+
+        if next_point == food {
+            loop {
+                let next_food = Point::random(width, height);
+                // Dont create food on snake
+                if !snake.contains(&next_food) {
+                    food = next_food;
+                    break;
+                }
+            }
+        } else {
+            snake.pop().unwrap();
         }
+
+        snake.insert(0, next_point);
+        sleep(Duration::from_millis(200));
     }
 }
