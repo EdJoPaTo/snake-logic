d4658d312cbcb1568d48b59820fde795583bab0b
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2024-05-07 01:30:26 +0200

    style: split import groups correctly

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 #![forbid(unsafe_code)]
 #![cfg_attr(not(feature = "std"), no_std)]
 
+pub use self::direction::{Direction, ALL_DIRECTIONS};
+pub use self::directions_possible::DirectionsPossible;
+pub use self::get_next_point::get_next_point;
+pub use self::point::Point;
+
 mod direction;
 mod directions_possible;
 mod get_next_point;
 mod point;
-
-pub use direction::{Direction, ALL_DIRECTIONS};
-pub use directions_possible::DirectionsPossible;
-pub use get_next_point::get_next_point;
-pub use point::Point;
